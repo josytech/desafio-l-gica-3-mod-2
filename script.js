a97fe733ee03930b930ce5177eaede3934f91cde
josytech <josytech4@gmail.com>
2022-01-24 23:31:19 -0300

    desafio lógica 3 mod

---
+++ script.js
@@ -0,0 +1,26 @@
+//  1
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+// 2
+
+i=10
+while (i>=1){
+console.log(i)
+i-=1
+}
+
+// 3
+let cont = 101
+while (1 <= cont ){
+    console.log(cont)
+    cont -= 2
+}
+
+
+
+// 4
+for (let a = 2; a <= 100; a+=2){
+    console.log(a)
+}
